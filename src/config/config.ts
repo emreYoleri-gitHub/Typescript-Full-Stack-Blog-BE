69e165b1cbfcc80d234c49a00f149559a59bcbf2
emreyoleri-gitHub <emreyoleri01@hotmail.com>
2021-11-20 10:44:16 +0300

    initial commit

---
+++ src/config/config.ts
@@ -0,0 +1,19 @@
+const config = {
+    mongo: {
+        options: {
+            useUnifiedTopology: true,
+            useNewUrlParser: true,
+            socketTimeoutMS: 30000,
+            keepAlive: false,
+            autoIndex: false,
+            retryWrites: false
+        },
+        url: `mongodb+srv://emreyoleriDB:1052emre@cluster0.lzsew.mongodb.net/blogDB`
+    },
+    server: {
+        host: 'heroku',
+        port: 5000
+    }
+};
+
+export default config;
